49e4bf143608da2fa10503b57e70e58cba56ca33
Krishna <chkrishna2001@live.com>
2022-05-19 05:48:04 -0400

    service

---
+++ TrainingService/TrainingService/Models/Contact.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TrainingService.Models
+{
+    public class Contact
+    {
+        [Key]
+        
+        public int ContactId { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public string? Email { get; set; }
+        public string? Phone { get; set; }
+        public string? Country { get; set; }
+    }
+}
